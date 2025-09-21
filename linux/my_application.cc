46786e134a47a5609a72f60db4766152179b1bb7
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2025-09-21 18:45:10 +1000

    Update window title on linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "magic_life_wheel");
+    gtk_header_bar_set_title(header_bar, "Magic Life Wheel");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "magic_life_wheel");
+    gtk_window_set_title(window, "Magic Life Wheel");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
